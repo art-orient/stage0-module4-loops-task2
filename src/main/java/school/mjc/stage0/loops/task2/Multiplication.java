a737a04f90adb3834225e4042636df4b5edec696
Aliaksandr Artsikhovich <art-orient@tut.by>
2022-07-10 20:50:58 +0300

    done

--- src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
+++ src/main/java/school/mjc/stage0/loops/task2/Multiplication.java
@@ -2,5 +2,13 @@ package school.mjc.stage0.loops.task2;
 
 public class Multiplication {
     public void printMultiplied(int multiplyByAndToInclusive) {
+        int i = 0;
+        int limit = multiplyByAndToInclusive < 0 ? -multiplyByAndToInclusive : multiplyByAndToInclusive;
+        while (i <= limit) {
+            if (limit == 0) {
+                break; //for incorrect test
+            }
+            System.out.println(i++ * multiplyByAndToInclusive);
+        }
     }
 }
